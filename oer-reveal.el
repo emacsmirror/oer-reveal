bfeab80df1392e2202e38963ffcfc7c3ab6a180b
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2020-03-13 09:26:59 +0100

    Turn oer-reveal-submodules-version into variable

--- oer-reveal.el
+++ oer-reveal.el
@@ -372,8 +372,15 @@ contained in this directory.")
 (defconst oer-reveal-submodules-url
   "https://gitlab.com/oer/emacs-reveal-submodules.git"
   "Git URL for submodules of reveal.js and plugins.")
-(defconst oer-reveal-submodules-version "1.2.0"
-  "Version of submodules to check out.")
+(defcustom oer-reveal-submodules-version "1.2.0"
+  "Version of submodules to check out.
+This can be a string, indicating a git version tag, or nil.
+If nil, `oer-reveal-submodules-ok-p' always returns t, and oer-reveal does
+not attempt to update the submodules, e.g., in Docker containers."
+  :group 'org-export-oer-reveal
+  :type '(choice (const nil) string)
+  :package-version '(oer-reveal . "2.5.0"))
+
 (defconst oer-reveal-buffer "*oer-reveal git output*"
   "Name of buffer holding Git output.")
 (defcustom oer-reveal-submodules-dir
