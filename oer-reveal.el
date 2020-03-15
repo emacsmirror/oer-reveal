ebeb4269113ee103dc9bec58d74a3973a2819789
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2020-03-15 18:09:58 +0100

    Update oer-reveal-submodules-version

--- oer-reveal.el
+++ oer-reveal.el
@@ -372,7 +372,7 @@ contained in this directory.")
 (defconst oer-reveal-submodules-url
   "https://gitlab.com/oer/emacs-reveal-submodules.git"
   "Git URL for submodules of reveal.js and plugins.")
-(defcustom oer-reveal-submodules-version "1.2.0"
+(defcustom oer-reveal-submodules-version "1.2.1"
   "Version of submodules to check out.
 This can be a string, indicating a git version tag, or nil.
 If nil, `oer-reveal-submodules-ok-p' always returns t, and oer-reveal does
