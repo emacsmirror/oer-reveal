41adf477d681d5d376aa1cdf0e0002edc7dd8aac
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2020-08-13 13:08:05 +0200

    Export notes to PDF by default
    My students prefer a more complete export.
    This requires org-re-reveal 3.1.0.

--- oer-reveal.el
+++ oer-reveal.el
@@ -8,7 +8,7 @@
 ;; Author: Jens Lechtenbörger
 ;; URL: https://gitlab.com/oer/oer-reveal
 ;; Version: 3.1.3
-;; Package-Requires: ((emacs "24.4") (org-re-reveal "3.0.0"))
+;; Package-Requires: ((emacs "24.4") (org-re-reveal "3.1.0"))
 ;; Keywords: hypermedia, tools, slideshow, presentation, OER
 
 ;; Emacs 24.4 adds advice-add and advice-remove, which are used below.
