440c6229013b41c7d8eceaccc3ba7778143fb097
Jens Lechtenbörger <lechten@wi.uni-muenster.de>
2025-03-19 15:54:53 +0100

    Version 4.29.0

--- oer-reveal.el
+++ oer-reveal.el
@@ -7,7 +7,7 @@
 
 ;; Author: Jens Lechtenbörger
 ;; URL: https://gitlab.com/oer/oer-reveal
-;; Version: 4.28.4
+;; Version: 4.29.0
 ;; Package-Requires: ((emacs "24.4") (org-re-reveal "3.32.0"))
 ;; Keywords: hypermedia, tools, slideshow, presentation, OER
 
